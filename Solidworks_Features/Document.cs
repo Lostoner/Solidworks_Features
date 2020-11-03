ffee9511e6fbd40dc940c7a936579145e77a3908
Lostoner <1151710764@qq.com>
2020-11-03 09:13:26 +0800

    update

---
+++ Solidworks_Features/Document.cs
@@ -0,0 +1,25 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solidworks_Features
+{
+    class Document
+    {
+        int numFea, numLoop, numPoint;
+
+        List<SketchPoint> Points = new List<SketchPoint>();
+        List<Feature> Feas = new List<Feature>();
+        List<Loop> Loops = new List<Loop>();
+
+
+        public static void printToDocument()
+        {
+            //把属性全部输出吧
+
+        }
+    }
+}
--- Solidworks_Features/newSketch.cs
+++ Solidworks_Features/newSketch.cs
@@ -4,30 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using SolidWorks.Interop.swconst;
-using System.Windows.Forms;
-using System.Diagnostics;
 
 namespace Solidworks_Features
 {
-
-    class Loop
+    class newSketch
     {
-        int num_poi;
-        int num_seg;
         List<SketchSegment> segs = new List<SketchSegment>();
         List<SketchPoint> pois = new List<SketchPoint>();
 
-        public Loop()
-        {
-
-        }
-
-        static public void store(Sketch ske)
+        public newSketch(Sketch ske)
         {
             object[] segments = ske.GetSketchSegments();
+            for(int i = 0; i < segments.Length; i++)
+            {
 
+            }
         }
     }
 }
